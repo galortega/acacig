98cc8d3646b3bf0acc371f2de4db899b1ecbf5cb
galortega <galfeort@espol.edu.ec>
2021-05-22 19:21:03 -0500

    primer commit - estructura del proyecto

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../assets/css/main.css";
+import "../assets/css/estilos.css";
+import "../assets/css/flexboxgrid.css";
+
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
